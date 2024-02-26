5f44d35166ec930bda90a84c96b24d6dc5d39361
Felipe Miotto <felipemiotto.564@gmail.com>
2024-02-26 14:34:38 -0300

    create screens

---
+++ src/screens/Contact.jsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';}
+
+export default function Contact() {
+  return (
+    <View>
+    </View>
+  );
+}
